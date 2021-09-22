7dacf81b567dfe6fa9ab0682d343030d23f8e9a3
Rohail Rafique <rohailrafique@gmail.com>
2021-09-22 16:46:51 -0400

    commit tree 612a0680a1dec45c7dadeb7ac07e9309ccb04d48

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+// it('returns a year the KC Chiefs won the superbowl', function () {
+//     expect(superbowlWin(record)).to.equal('1969');
+//   });
+
+
+it('returns a year the KC Chiefs won the superbowl', function () {
+    expect(superbowlWin(record)).to.equal('1969');
+  });
+
+  it('returns undefined if the record has no win objects', function() {
+    const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+    expect(superbowlWin(sadReality)).to.equal(undefined)
+  });
+
+
+function superbowlWin(result, finder) {
+
+  for(let item of result){if(finder(item) === 'W') {
+    return year
+    break
+  }
+
+  }
+
+}
